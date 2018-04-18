4c52fa6bd809dcfec245ec684dd693e4a7bf128a
sandeepcn <sreecn1@gmail.com>
2018-04-18 22:01:34 +0530

    Added task entity

---
+++ BusinessEntities/TaskModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessEntities
+{
+    public class TaskModel
+    {
+        public long Task_ID { get; set; }
+
+        public long? Parent_ID { get; set; }
+
+        public string Parent_Name { get; set; }
+
+        public long? Project_ID { get; set; }
+        public string Project_Name { get; set; }
+        public string TaskName { get; set; }
+        public DateTime? Start_Date { get; set; }
+
+        public DateTime? End_Date { get; set; }
+
+        public short? Priority { get; set; }
+
+        public bool? Status { get; set; }
+
+        public long? User_ID { get; set; }
+        public string User_Name { get; set; }
+    }
+}

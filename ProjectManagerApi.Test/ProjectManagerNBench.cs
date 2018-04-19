9d0c3f657229ee02f54588b16ce63a749e4f3b62
sandeepcn <38332130+sandeepcn@users.noreply.github.com>
2018-04-19 21:09:51 +0530

    NBench
    NBench

--- ProjectManagerApi.Test/ProjectManagerNBench.cs
+++ ProjectManagerApi.Test/ProjectManagerNBench.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NBench;
+using ProjectManagerApi.Controllers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,35 @@ namespace ProjectManagerApi.Test
 {
     class ProjectManagerNBench
     {
+        private const string AddCounterName = "AddCounter";
+        private Counter addCounter;
+        private int key;
+
+        private const int AcceptableMinAddThroughput = 100;
+
+        [PerfSetup]
+        public void Setup(BenchmarkContext context)
+        {
+            addCounter = context.GetCounter(AddCounterName);
+            key = 0;
+        }
+        [PerfBenchmark(NumberOfIterations = 10, RunMode = RunMode.Throughput, RunTimeMilliseconds = 10000, TestMode = TestMode.Measurement)]
+        [CounterMeasurement(AddCounterName)]
+        [CounterThroughputAssertion(AddCounterName, MustBe.GreaterThan, AcceptableMinAddThroughput)]
+        public void GetAllUsersCounterThroughputBenchMark(BenchmarkContext context)
+        {
+            UserController oController = new UserController();
+            oController.Get();
+            addCounter.Increment();
+        }
+        //[PerfBenchmark(Description = "You can write your description here.",
+        //    NumberOfIterations = 500, RunMode = RunMode.Throughput, RunTimeMilliseconds = 10000000, TestMode = TestMode.Measurement)]
+        //[MemoryAssertion(MemoryMetric.TotalBytesAllocated, MustBe.LessThanOrEqualTo, ByteConstants.SixtyFourKb)]
+        //public void GetAllUsersMemoryBenchMark()
+        //{
+        //    UserController oController = new UserController();
+        //    oController.Get();
+        //    addCounter.Increment();
+        //}
     }
 }

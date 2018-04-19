1a047282adc6dc5ae75fe38e09bba6a01f33c82d
sandeepcn <38332130+sandeepcn@users.noreply.github.com>
2018-04-19 20:40:31 +0530

    Test data from file
    Test data from file

--- ProjectManagerApi.Test/UserControllerTest.cs
+++ ProjectManagerApi.Test/UserControllerTest.cs
@@ -5,17 +5,48 @@ using System.Text;
 using System.Threading.Tasks;
 using ProjectManagerApi.Controllers;
 using NUnit.Framework;
+using BusinessEntities;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using Newtonsoft.Json;
+using System.Web.Script.Serialization;
+using System.Collections;
+using System.Reflection;
+
 namespace ProjectManagerApi.Test
 {
     [TestFixture]
     public class UserControllerTest
     {
-        [Test]
-        public void TestGetAllUser()
+        public static IEnumerable GetTestDataUser
+        {
+            get
+            {
+                string FileLoc = @"TestData\AddUser.json";
+                string FilePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase).Replace("file:\\","").Replace("\\bin\\Debug", "");
+
+                var jsonText = File.ReadAllText(Path.Combine(FilePath,FileLoc));
+                var adduser = JsonConvert.DeserializeObject<UserModel>(jsonText);
+                yield return adduser;
+            }
+        }
+
+        [Test, TestCaseSource("GetTestDataUser")]
+        public void TestAddUser(UserModel testUser)
         {
             UserController oController = new UserController();
-            //Assert.AreEqual(0, oController.Get().Count());
-
+            Assert.AreEqual("User added successfully", oController.Post(testUser).status.Message);
         }
+
+        public static UserModel GetAddUserTestData()
+        {
+            string FileLoc = @"C:\Users\Admin\Documents\PM2\ProjectManagerApi\ProjectManagerApi.Test\TestData\AddUser.json";
+
+            var jsonText = File.ReadAllText(FileLoc);
+            var adduser = JsonConvert.DeserializeObject<UserModel>(jsonText);
+            return adduser;
+        }
+
+        
     }
 }

9c1f6e39465bfd56c7065db9332bfd8f961334ce
sandeepcn <38332130+sandeepcn@users.noreply.github.com>
2018-04-23 19:57:47 +0530

    include parent task
    include parent task

--- DataAccessLayer/TaskRepository.cs
+++ DataAccessLayer/TaskRepository.cs
@@ -8,20 +8,20 @@ using System.Data.Entity;
 
 namespace DataAccessLayer
 {
-   public class TaskRepository
+    public class TaskRepository
     {
         public List<Task> GetAllTasks()
         {
             using (var context = new ProjectManagerContext())
             {
-               return context.Tasks.Include(x=>x.User).Include(x=>x.Project).ToList();
+                return context.Tasks.Include(x => x.User).Include(x => x.Project).Include(x => x.ParentTask).ToList();
             }
         }
         public Task AddTask(Task oTask)
         {
             using (var context = new ProjectManagerContext())
             {
-                oTask= context.Tasks.Add(oTask);
+                oTask = context.Tasks.Add(oTask);
                 context.SaveChanges();
                 return oTask;
             }
@@ -30,12 +30,12 @@ namespace DataAccessLayer
         {
             using (var context = new ProjectManagerContext())
             {
-                oTask= context.Tasks.Attach(oTask);
+                oTask = context.Tasks.Attach(oTask);
                 context.Entry(oTask).State = EntityState.Modified;
                 context.SaveChanges();
                 return oTask;
             }
         }
-        
+
     }
 }
